2e030c29365728bb579f30fd851930400f73b138
askirmas <kirmas@phystech.edu>
2020-03-30 01:33:23 +0300

    Update bencher to run Record of functions

--- bencher.ts
+++ bencher.ts
@@ -5,18 +5,19 @@ export {
 }
 
 function bencher<I extends any[], O>(fns: Record<string|number, (...args: I) => O>, args: Record<string, I>) {
-  const functions = Object.values(fns);
-
   for (const argName in args) {
     const runner = new Suite()
     
-    for (const fn of functions)
+    for (const fnName in fns) {
+      const fn = fns[fnName]
       runner.add(
-        `${fn.name}(${argName})`,
-        () =>
+        `${fn.name ?? fnName}(${argName})`,
+        () => {
+          //@ts-ignore
           fn(...args[argName as keyof typeof args] as Parameters<typeof fn>)
+        }
       )
-
+    }
     runner
     .on('cycle', ({target}: any) => console.log(`${target}`))
     .on('complete', function(this: Suite) {

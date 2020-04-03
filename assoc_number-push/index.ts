09ce8d368c3563719fbbc359f453c25d978f46b6
askirmas <kirmas@phystech.edu>
2020-04-03 17:13:36 +0300

    Add fill methods as push

--- assoc_number-push/index.ts
+++ assoc_number-push/index.ts
@@ -1,31 +1,58 @@
 export {
-  objectAssign, arrayAssign, arrayPush, mapSet
+  objectAssign,
+  arrayAssign, arrayPush, arrayFill, arrayAssignRev,
+  mapSet, mapRange
 }
 
-function objectAssign(count: number) {
+function objectAssign(count: number, value = 0) {
   const $return: Record<number, any> = {}
   for (let i = 0; i < count; i++)
-    $return[i] = 0
+    $return[i] = value
   return $return
 }
 
-function arrayAssign(count: number) {
+function arrayAssign(count: number, value = 0) {
   const $return: Record<number, any> = []
   for (let i = 0; i < count; i++)
-    $return[i] = 0
+    $return[i] = value
   return $return
 }
 
-function arrayPush(count: number) {
+function arrayAssignRev(count: number, value = 0) {
+  const $return: Record<number, any> = []
+  for (let i = count; i --;)
+    $return[i] = value
+  return $return
+}
+
+function arrayPush(count: number, value = 0) {
   const $return = []
   for (let i = 0; i < count; i++)
-    $return.push(0)
+    $return.push(value)
   return $return
 }
 
-function mapSet(count: number) {
+function arrayFill(count: number, value = 0) {
+  return new Array(count).fill(value)
+}
+
+function mapSet(count: number, value = 0) {
   const $return: Map<number, any> = new Map()
   for (let i = 0; i < count; i++)
-    $return.set(i, 0)
+    $return.set(i, value)
   return $return
+}
+
+function mapRange(count: number, value = 0) {
+  //@ts-ignore
+  return new Map(rangeEntries(count, value))
+}
+
+function* rangeEntries(max: number, value: any) {
+  for (let i = 0; i < max; i++)
+    yield newArguments(i, value)
+}
+
+function newArguments(..._: any) {
+  return arguments
 }
